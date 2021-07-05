ae24499afd6186527a24b4f36cf574ea2b18ea04
dato.qurasbediani86 <dato.qurasbediani86@gmail.com>
2021-07-06 03:06:40 +0400

    order list fix

--- src/js/app/cart/OrderConfirmPopup.jsx
+++ src/js/app/cart/OrderConfirmPopup.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useContext} from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import { useForm } from 'react-hook-form';
 import { UserContext } from "../../contexts/UserContext";
@@ -6,8 +6,8 @@ import SelectInput from '../inputs/SelectInput';
 import AddUserCard from '../card/AddUserCard';
 import { useParams } from 'react-router-dom';
 
-export default function ({cart, setOrderConfirmPopup, setExitAnimation}) {
-  const {register, handleSubmit, control, getValues, formState: { errors }} = useForm();
+export default function ({ cart, setOrderConfirmPopup, setExitAnimation }) {
+  const { register, handleSubmit, control, getValues, formState: { errors } } = useForm();
   const [addCard, setAddCard] = useState(false);
   const [confirmed, setConfirmed] = useState(false);
   const [cards, setCards] = useState([]);
@@ -17,7 +17,8 @@ export default function ({cart, setOrderConfirmPopup, setExitAnimation}) {
   const tableId = 1;
 
   const FetchCards = async () => {
-    await axios({ method:'get', url : "https://localhost:44353/v1/card",
+    await axios({
+      method: 'get', url: "https://localhost:44353/v1/card",
       params: {
       },
       headers: {
@@ -25,33 +26,34 @@ export default function ({cart, setOrderConfirmPopup, setExitAnimation}) {
         Authorization: `Bearer ${userContext.accessToken}`
       }
     }).then(res => {
-      console.log('result:',res);
+      console.log('result:', res);
       setCards(res.data.data);
     });
   }
-  
-  useEffect(()=>{
+
+  useEffect(() => {
     FetchCards()
-  },[]);
+  }, []);
 
   const onSubmit = (data, e) => {
     data.shoppingCartItems = cart.cartItems;
     console.log(data);
-    axios({ method:'POST', url : "https://localhost:44353/v1/order", config :{
+    axios({
+      method: 'POST', url: "https://localhost:44353/v1/order",
       params: {
         tenantId: cart.tenantId,
         tableId: tableId,
-      }, 
+      },
       headers: {
         'Access-Control-Allow-Origin': '*',
         Authorization: `Bearer ${userContext.accessToken}`
       }
-    }}).then(res => {
-        if (res.status === 201) {
-          e.target.reset();
-        } else {
-          console.log(res);
-        }
+    }).then(res => {
+      if (res.status === 201) {
+        e.target.reset();
+      } else {
+        console.log(res);
+      }
     })
   }
 
@@ -59,58 +61,58 @@ export default function ({cart, setOrderConfirmPopup, setExitAnimation}) {
   return (
     <div className='fixed-bg client-side'>
       <div className="order-confirm pt-5 pb-4 popup position-absolute alert alert-light order-confirm popup">
-        <button 
-          className='close-btn' 
-          onClick={()=>setOrderConfirmPopup(false)}
+        <button
+          className='close-btn'
+          onClick={() => setOrderConfirmPopup(false)}
         >
           <i className="fas fa-times"></i>
         </button>
         {
-          confirmed===false ?
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="cards">
-              <SelectInput
-                label="Choose card "
-                formGroupClassName='full-width'
-                name="card"
-                optionLabel = "numberMask"
-                control={control}
-                options={cards}
-                getValues={getValues}
-                optionValue='id'
-                register={register}
-                errorMessage={errors?.card?.message}
+          confirmed === false ?
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="cards">
+                <SelectInput
+                  label="Choose card "
+                  formGroupClassName='full-width'
+                  name="card"
+                  optionLabel="numberMask"
+                  control={control}
+                  options={cards}
+                  getValues={getValues}
+                  optionValue='id'
+                  register={register}
+                  errorMessage={errors?.card?.message}
+                />
+              </div>
+              <div className='btn btn-secondary btn-sm mb-4' onClick={() => setAddCard(true)}>
+                Add New Card
+              </div>
+              <div className="total mb-3">
+                Total: <span className='font-weight-bold big'>{cart.orderTotal + cart.tax} &#8382;</span>
+              </div>
+              <input
+                type="submit"
+                className="btn btn-block btn-outline-success confirm"
+                // onClick={()=>setConfirmed(true)}
+                value='CONFIRM ORDER'
               />
-            </div>
-            <div className='btn btn-secondary btn-sm mb-4' onClick={()=>setAddCard(true)}>
-              Add New Card
-            </div>
-            <div className="total mb-3">
-              Total: <span className='font-weight-bold big'>{cart.orderTotal + cart.tax} &#8382;</span>
-            </div>
-            <input 
-              type="submit"
-              className="btn btn-block btn-outline-success confirm"
-              // onClick={()=>setConfirmed(true)}
-              value='CONFIRM ORDER'
-            />
-          </form> :
-          <>
-          <div className='d-flex flex-column'>
-            <i className="fas fa-check-circle btn-outline-success fa-check-circle fas larger mx-auto"></i>
-            <h4 className='mb-5 mt-3 text-center'>
-              Payment confirmed and Order received by restaurant 
-            </h4>
-            <div 
-              className="btn btn-block btn-outline-success confirm"
-              onClick={()=>{
-                setOrderConfirmPopup(false); 
-                setExitAnimation(1);
-              }}
-            >Ok
-            </div>
-          </div>
-          </>
+            </form> :
+            <>
+              <div className='d-flex flex-column'>
+                <i className="fas fa-check-circle btn-outline-success fa-check-circle fas larger mx-auto"></i>
+                <h4 className='mb-5 mt-3 text-center'>
+                  Payment confirmed and Order received by restaurant
+                </h4>
+                <div
+                  className="btn btn-block btn-outline-success confirm"
+                  onClick={() => {
+                    setOrderConfirmPopup(false);
+                    setExitAnimation(1);
+                  }}
+                >Ok
+                </div>
+              </div>
+            </>
         }
       </div>
       {
